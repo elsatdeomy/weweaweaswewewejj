40304b5c2f5167bbbc9677d99f2301406636754b
elsatdeomy <42783125+elsatdeomy@users.noreply.github.com>
2018-08-30 04:23:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.'
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`OF A7 OF A7`,"http://twitch.tv/S-F")
@@ -26,16 +25,6 @@ client.user.setGame(`OF A7 OF A7`,"http://twitch.tv/S-F")
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
-});
-
-client.login('NDc2NTU0MjY1MDIwODU4Mzgw.DkyZ9A.5-o1ZluEjfNyVDeGUcRv2auBtzw');
-client.on('message', message => {
-                if(message.content === "B.inv") {
-                    let embed = new Discord.RichEmbed ()
-                    embed.setTitle("**:arrow_right: Invite Battellions Bot!**")
-                    .setURL("https://discordapp.com/oauth2/authorize?client_id=476554265020858380&permissions=0&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D476554265020858380%26permissions%3D8%26scope%3Dbot&scope=bot");
-                   message.channel.sendEmbed(embed);
-                  }
 });
 
 client.login(process.env.BOT_TOKEN);
